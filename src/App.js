18d1b32f86c9d9ed1b02f721dc81dca267882b8c
Nitin <nitinjadhav@Nitins-MacBook-Pro.local>
2018-02-05 10:01:55 +0530

    Adds 1. new font, 2. optimized tailwind configuraion, 3. first mobile view 4. post-css to process tailwind config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./styles/compiled.css";
+import "./App.css";
+import "./styles/fonts/FiraCode-Regular.woff";
+import "./styles/fonts/FiraCode-Bold.woff";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header p-4 text-center">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title font-fira-code font-light">
+            CSS Builder{" "}
+            <sup class="text-xs" style={{ top: "-1.5em" }}>
+              0.1
+            </sup>
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="output bg-grey p-4">
+          <span class="absolute pin-t pin-r">Output</span>
+        </div>
+        <div className="options p-4">Options</div>
+        <div className="code bg-grey p-4">Code</div>
       </div>
     );
   }
